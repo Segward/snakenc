622bb27f01ac822909c0d5f0e45b43e7212ebb04
Segward <gustavhaverstad@gmail.com>
2025-05-31 21:09:50 +0200

    Move player around

--- src/snakenc.cpp
+++ src/snakenc.cpp
@@ -25,35 +25,50 @@ int main(int argc, char **argv) {
     int winY = (yMax - winH) / 2;
     int winX = (xMax - winW) / 2;
 
-    // Create the main window
-    WINDOW *mWin = newwin(winH, winW, winY, winX);
-    box(mWin, 0, 0);
-    wbkgd(mWin, COLOR_PAIR(1));
-    wrefresh(mWin);
-
     // Make sizes for the sub window
-    int subH = 2;
-    int subW = 3;
+    int subH = 1;
+    int subW = 2;
     int subY = (winH - subH) / 2;
     int subX = (winW - subW) / 2;
 
-    // Create the sub window
-    WINDOW *sWin = derwin(mWin, subH, subW, subY, subX);
-    box(sWin, 0, 0);
-    wbkgd(sWin, COLOR_PAIR(2));
-    wrefresh(sWin);
-    
+    // Main game loop
     int ch;
     while (1) {
+      clear();
+      refresh();
+
+      // Create the main window
+      WINDOW *mWin = newwin(winH, winW, winY, winX);
+      box(mWin, 0, 0);
+      wbkgd(mWin, COLOR_PAIR(1));
+      wrefresh(mWin);
+
+      // Create the sub window
+      WINDOW *sWin = derwin(mWin, subH, subW, subY, subX);
+      box(sWin, 0, 0);
+      wbkgd(sWin, COLOR_PAIR(2));
+      wrefresh(sWin);
+
       ch = getch();
-      
-      if (ch == KEY_DOWN) {
-      
+      if (ch == KEY_DOWN && subY < winH - subH - 1) {
+        subY++; 
       } 
-   
+      
+      if (ch == KEY_UP && subY > 1) {
+        subY--;
+      }
+
+      if (ch == KEY_LEFT && subX > 1) {
+        subX--;
+      }
+
+      if (ch == KEY_RIGHT && subX < winW - subW - 1) {
+        subX++;
+      }
+
+      refresh();
     }
 
     endwin();
     return 0;
 }
-
